41b270570e2703a0073d6e087f8d7cb35d2d85a7
carrpenoctem <dn.mski@gmail.com>
2019-06-08 18:00:43 +0200

    Initialization

---
+++ src/main/java/com/dmodzelewski/binarydecimalconverter/Main.java
@@ -0,0 +1,24 @@
+package com.dmodzelewski.binarydecimalconverter;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+/**
+ * The purpose of this app is to convert binary numbers to decimal and the
+ * opposite.
+ *
+ * @author D.Modzelewski
+ */
+public class Main {
+
+    /* Logger, for logging errors */
+    private static final Logger classLogger = Logger.getLogger(Main.class.getCanonicalName());
+    /* A convenient way to receive input from the user */
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
